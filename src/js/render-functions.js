be7d14875241b58c7d336b02d86072a11edb3fe3
IvonLagta <32240085+IvonLagta@users.noreply.github.com>
2026-02-18 12:22:13 +0100

    Refactor loader handling and improve code structure

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,7 +2,9 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
+const loader = document.querySelector(".loader");
 const loaderText = document.querySelector('.loader-text');
+
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -43,3 +45,4 @@ export function showLoader() {
 export function hideLoader() {
   loaderText.classList.add('hidden');
 }
+
